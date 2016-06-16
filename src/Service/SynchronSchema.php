76f13e8494eb6f520d9c60cd3767f7ee5a4e0626
Luis SEVERINO <lfs.severino@gmail.com>
2016-06-16 11:42:10 +0200

    lsynchron service

---
+++ src/Service/SynchronSchema.php
@@ -0,0 +1,43 @@
+<?php
+
+  // TODO: USE THIS AS SERVICE
+  namespace Drupal\synchron\Service;
+
+  class SynchronSchema {
+
+    protected $defaultConnectionOptions;
+
+    public function __construct() {
+      $getConnectionOptions = \Drupal::service('database')->getConnectionOptions();
+
+      // Store default connected database
+      $this->setDefaultConnectionOptions($getConnectionOptions);
+    }
+
+    protected function setDefaultConnectionOptions($connectionOptions) {
+      $this->defaultConnectionOptions = $connectionOptions;
+    }
+
+    public function getDefaultConnectionOptions() {
+      return $this->defaultConnectionOptions;
+    }
+
+    function setConnectionDatabase($databaseName) {
+
+      // Get active current database service connection
+      $getConnectionOptions = $this->getDefaultConnectionOptions();
+
+      // Choose a database name
+      // Use an admin form to handle the database name liste
+      // Override default database name only because
+      // This will work on the same server only
+      $getConnectionOptions['database'] = $databaseName;
+
+      // Return the good PDO object to this connection
+      $pdoConnection = \Drupal::service('database')->open($getConnectionOptions);
+
+      // Override the service by passing new values to connection construct
+      \Drupal::service('database')->__construct($pdoConnection, $getConnectionOptions);
+    }
+
+  }
