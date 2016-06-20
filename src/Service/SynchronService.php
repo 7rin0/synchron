aa48727552cb2ad1544ccd69e7ef1b1d1caf99d8
Luis SEVERINO <lfs.severino@gmail.com>
2016-06-20 12:40:10 +0200

    delete revisions

--- src/Service/SynchronService.php
+++ src/Service/SynchronService.php
@@ -7,30 +7,24 @@
 
   class SynchronService {
 
+    protected $serviceDatabase;
     protected $defaultConnectionOptions;
 
     public function __construct() {
-      $getConnectionOptions = \Drupal::service('database')->getConnectionOptions();
-
-      // Store default connected database
-      $this->setDefaultConnectionOptions($getConnectionOptions);
+      $this->serviceDatabase = \Drupal::service('database');
+      $this->defaultConnectionOptions = $this->serviceDatabase->getConnectionOptions();
     }
 
-    protected function setDefaultConnectionOptions($connectionOptions) {
-      $this->defaultConnectionOptions = $connectionOptions;
-    }
-
+    // Get Default database connection
     public function getDefaultConnectionOptions() {
       return $this->defaultConnectionOptions;
     }
 
+    // TODO Stock connection one and two to avoid open new one each time
     public function setConnectionDatabase($databaseName) {
 
       // Get active current database service connection
       $getConnectionOptions = $this->getDefaultConnectionOptions();
-
-      // Service database
-      $databaseService = \Drupal::service('database');
 
       // Choose a database name
       // Use an admin form to handle the database name liste
@@ -39,10 +33,10 @@
       $getConnectionOptions['database'] = $databaseName;
 
       // Return the good PDO object to this connection
-      $pdoConnection = $databaseService->open($getConnectionOptions);
+      $pdoConnection = $this->serviceDatabase->open($getConnectionOptions);
 
       // Override the service by passing new values to connection construct
-      $databaseService->__construct($pdoConnection, $getConnectionOptions);
+      $this->serviceDatabase->__construct($pdoConnection, $getConnectionOptions);
 
       // CHeck if module exists
       $this->moduleHandler();
@@ -63,15 +57,16 @@
     }
 
     public function provisionFromSiteToAnother($nid, $fromDatabase, $toDatabase) {
-
       // Set database connection to $fromDatabase
       $this->setConnectionDatabase($fromDatabase);
-      $loadNodeThisDatabase = $this->loadNode($nid, 'nid');
 
       // If entity exists continue
-      if($loadNodeThisDatabase) {
+      if($loadNodeThisDatabase = $this->loadNode($nid, 'nid')) {
+        $nodeThisDatabase = $loadNodeThisDatabase->toArray();
+        $originalRevisions = $this->getRevisions($loadNodeThisDatabase);
+
         // If entity hasnt synchronid add new one
-        if(!$syncronid = $loadNodeThisDatabase->get('synchronid')->getValue()) {
+        if(!$syncronid = $nodeThisDatabase['synchronid']) {
           $syncronid = uniqid();
           $loadNodeThisDatabase->set('synchronid', $syncronid)->save();
         } else {
@@ -84,7 +79,12 @@
 
         // Load node by synchronid to match the target node
         if($loadNodeTargetDatabase = $this->loadNode($syncronid, 'synchronid')) {
+          // Delete target revisions
+          $this->deleteRevisions($loadNodeTargetDatabase);
           // TODO replace target data with origin data
+          // Delete target revisions
+          // Replace target fields except nid
+          // TODO when provisionning check if theres related entities do rovision aswell
           echo 'found';
         } else {
           $loadNodeTargetDatabase = $loadNodeThisDatabase->createDuplicate()->setOriginalId();
@@ -100,13 +100,12 @@
     }
 
     protected function moduleHandler() {
-      $databaseConnectionService = \Drupal::service('database');
       $moduleHandlerService = \Drupal::service('module_handler');
       $moduleInstallerService = \Drupal::service('module_installer');
       // Check if module exists
       $moduleExists = $moduleHandlerService->moduleExists('synchron');
       // Check if module is enabled
-      $isSynchronEnabled = $databaseConnectionService->select('key_value', 'kv')
+      $isSynchronEnabled = $this->serviceDatabase->select('key_value', 'kv')
         ->condition('kv.name', 'synchron')
         ->fields('kv')
         ->execute();
@@ -117,6 +116,31 @@
       }
 
       return $moduleExists;
+    }
+
+    // Delete all revisions to a given node
+    protected function deleteRevisions($targetNode) {
+      $entityManagerService = $node_revision = \Drupal::entityTypeManager();
+      $getEntityRevisions = $this->serviceDatabase->delete('node_revision')
+        ->condition('revision_uid', $targetNode->get('revision_uid')->getValue()[0]['target_id'])
+        ->execute();
+    }
+
+    // Return all revisions to a given node
+    protected function getRevisions($originalNode) {
+      $entityManagerService = $node_revision = \Drupal::entityTypeManager();
+      $getEntityRevisions = $this->serviceDatabase->select('node_revision', 'nr')
+        ->condition('nr.revision_uid', $originalNode->get('revision_uid')->getValue()[0]['target_id'])
+        ->fields('nr')
+        ->execute();
+      $originalRevisions = $getEntityRevisions->fetchAll(\PDO::FETCH_OBJ);
+
+      // Load Original Revisions entities
+      $originalRevisionEntity = [];
+      foreach ($originalRevisions as $key => $value) {
+        // TODO dynamique load by entity instead only node based
+        $originalRevisionEntity[] = $entityManagerService->getStorage('node')->loadRevision($value->vid)->toArray();
+      }
     }
 
     public function getEntity() {
